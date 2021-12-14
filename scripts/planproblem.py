209c4933614ebd0685f8c9a26dc2c43da5fa6318
ZoeBetta <77151364+ZoeBetta@users.noreply.github.com>
2021-12-14 11:24:25 +0000

    replan implemented
    actions all simulated. missing the real actions

--- scripts/planproblem.py
+++ scripts/planproblem.py
@@ -30,13 +30,13 @@ from rosplan_knowledge_msgs.srv import *
 from std_srvs.srv import Empty, EmptyResponse
 from rosplan_interface_mapping.srv import CreatePRM
 from rosplan_dispatch_msgs.srv import DispatchService, DispatchServiceResponse, PlanningService, PlanningServiceResponse
+from rosplan_knowledge_msgs.srv import KnowledgeUpdateService, KnowledgeUpdateServiceRequest
 from diagnostic_msgs.msg import KeyValue
 import time
 
-def main():
-    global pub_, active_, act_s
-    #I initialize the goal
-    rospy.init_node('planproblem')
+
+def initialization():
+    global problem_generation, planning, parsing, dispatch, update
     rospy.wait_for_service('/rosplan_problem_interface/problem_generation_server')
     problem_generation = rospy.ServiceProxy('/rosplan_problem_interface/problem_generation_server',Empty)
     rospy.wait_for_service('/rosplan_planner_interface/planning_server')
@@ -44,22 +44,61 @@ def main():
     rospy.wait_for_service('/rosplan_parsing_interface/parse_plan')
     parsing = rospy.ServiceProxy('/rosplan_parsing_interface/parse_plan',Empty)
     rospy.wait_for_service('/rosplan_plan_dispatcher/dispatch_plan')
-    dispatch = rospy.ServiceProxy('/rosplan_plan_dispatcher/dispatch_plan',DispatchService)
+    dispatch = rospy.ServiceProxy('/rosplan_plan_dispatcher/dispatch_plan',DispatchService)	
+    rospy.wait_for_service('/rosplan_knowledge_base/update')
+    update= rospy.ServiceProxy('/rosplan_knowledge_base/update', KnowledgeUpdateService)
     print('all servers loaded')
-    response_pg=problem_generation()
-    print('problem generates')
-    time.sleep(5)
-    response_pl=planning()
-    print('plan generates')
-    time.sleep(5)
-    response_pars=parsing()
-    print('parse generates')
-    time.sleep(5)
-    response_dis=dispatch()
-    print('dispatch generates')
-    time.sleep(5)
+
+def update_waypoint(name):
+    req=KnowledgeUpdateServiceRequest()
+    req.update_type=2
+    req.knowledge.is_negative=False
+    req.knowledge.knowledge_type=1
+    req.knowledge.attribute_name= 'hint_taken'
+    req.knowledge.values.append(diagnostic_msgs.msg.KeyValue('waypoint', name))	
+    result=update(req)
+    print(result)
     
-    rospy.spin()
+def update_complete():
+    req=KnowledgeUpdateServiceRequest()
+    req.update_type=2
+    req.knowledge.is_negative=False
+    req.knowledge.knowledge_type=1
+    req.knowledge.attribute_name= 'hypothesis_complete'
+    result=update(req)	
+
+def know_update():
+    update_waypoint('wp1')
+    update_waypoint('wp2')
+    update_waypoint('wp3')
+    update_waypoint('wp4')
+
+def main():
+    global pub_, active_, act_s
+    #I initialize the goal
+    rospy.init_node('planproblem')
+    initialization()
+    success=False
+    goal=False
+    
+    while ( success== False or goal == False):
+        response_pg=problem_generation()
+        print('problem generates')
+        time.sleep(1)
+        response_pl=planning()
+        print('plan generates')
+        time.sleep(1)
+        response_pars=parsing()
+        print('parse generates')
+        time.sleep(1)
+        response_dis=dispatch()
+        print(response_dis.success)
+        print(response_dis.goal_achieved)
+        success= response_dis.success
+        goal= response_dis.goal_achieved
+        know_update()
+        time.sleep(1)
+    print ( 'SUCCESS')
 
 if __name__ == '__main__':
     main()
