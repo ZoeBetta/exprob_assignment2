d3aeb493cf64a807dba280c554f0e8c7fd0568e3
ZoeBetta <77151364+ZoeBetta@users.noreply.github.com>
2022-02-27 21:19:08 +0000

    miss hint elaboration

--- src/Hint.cpp
+++ src/Hint.cpp
@@ -6,7 +6,55 @@
 #include <cstdlib>
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group_interface.h>
-int i=0;
+#include <exprob_assignment2/ErlOracle.h>
+int received=0;
+int previous=1;
+void hintCallback( const exprob_assignment2::ErlOracle x)
+{
+	received=1;
+}
+int movehigh()
+{
+	geometry_msgs::Pose target_pose1;
+moveit::planning_interface::MoveGroupInterface group("arm");
+group.setEndEffectorLink("cluedo_link");
+group.setPoseReferenceFrame("arm_base_link");
+group.setPlannerId("RRTstar");
+group.setNumPlanningAttempts(10);
+group.setPlanningTime(10.0);
+group.allowReplanning(true);
+group.setGoalPositionTolerance(0.01);
+	target_pose1.position.x = 0.0;
+	target_pose1.position.y = 0.5;
+	target_pose1.position.z = 1.25;
+	group.setStartStateToCurrentState();
+	group.setApproximateJointValueTarget(target_pose1, "cluedo_link");
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	group.plan(my_plan);
+	group.execute(my_plan);
+	return 0;
+}
+int movelow()
+{
+	geometry_msgs::Pose target_pose1;
+moveit::planning_interface::MoveGroupInterface group("arm");
+group.setEndEffectorLink("cluedo_link");
+group.setPoseReferenceFrame("arm_base_link");
+group.setPlannerId("RRTstar");
+group.setNumPlanningAttempts(10);
+group.setPlanningTime(10.0);
+group.allowReplanning(true);
+group.setGoalPositionTolerance(0.01);
+	target_pose1.position.x = 0.0;
+	target_pose1.position.y = 0.5;
+	target_pose1.position.z = 0.75;
+	group.setStartStateToCurrentState();
+	group.setApproximateJointValueTarget(target_pose1, "cluedo_link");
+	moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+	group.plan(my_plan);
+	group.execute(my_plan);
+	return 0;
+}
 namespace KCL_rosplan {
 HintInterface::HintInterface(ros::NodeHandle &nh) {
 // here the initialization
@@ -14,45 +62,85 @@ HintInterface::HintInterface(ros::NodeHandle &nh) {
 bool HintInterface::concreteCallback(const rosplan_dispatch_msgs::ActionDispatch::ConstPtr& msg) {
 // here the implementation of the action 
 std::cout << "Moving to get the hint " << std::endl;
-geometry_msgs::Pose target_pose1;
-moveit::planning_interface::MoveGroupInterface group("arm");
-group.setEndEffectorLink("cluedo_link");
-group.setPoseReferenceFrame("arm_base_link");
-group.setPlannerId("RRTstar");
-group.setNumPlanningAttempts(10);
-group.setPlanningTime(5.0);
-group.allowReplanning(true);
-group.setGoalJointTolerance(0.0001);
-group.setGoalPositionTolerance(0.0001);
-group.setGoalOrientationTolerance(0.001);
 
-if(msg->parameters[0].value == "wp1"){
-target_pose1.position.x = 0.0;
-target_pose1.position.y = 0.5;
-target_pose1.position.z = 0.75;
-}
-else if (msg->parameters[0].value == "wp2"){
-target_pose1.position.x = 0.0;
-target_pose1.position.y = 0.5;
-target_pose1.position.z = 1.25 ;
-}
-else if (msg->parameters[0].value == "wp3"){
-target_pose1.position.x = 0.0;
-target_pose1.position.y = 0.5;
-target_pose1.position.z = 0.75;
-}
-else if (msg->parameters[0].value == "wp4"){
-target_pose1.position.x = 0.0;
-target_pose1.position.y = 0.5;
-target_pose1.position.z = 1.25;
-}
 
-group.setStartStateToCurrentState();
-group.setApproximateJointValueTarget(target_pose1, "cluedo_link");
-moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-group.plan(my_plan);
-group.execute(my_plan);
-ROS_INFO("Action (%d) performed: completed!", i);
+if(msg->parameters[0].value == "wp1")
+	{
+		while(received==0)
+			{
+				if (previous==1)
+					{
+						movelow();
+						previous=0;
+					}
+				else if (previous ==0)
+					{
+						movehigh();
+						previous=1;
+					}
+			}
+		// elabora indizio
+		received=0;
+			
+	}
+else if (msg->parameters[0].value == "wp2")
+	{
+		while (received==0)
+			{
+				if (previous==1)
+					{
+						movelow();
+						previous=0;
+					}
+				else if (previous ==0)
+					{
+						movehigh();
+						previous=1;
+					}
+			}
+		// elabora indizio
+		received=0;
+			
+	}
+else if (msg->parameters[0].value == "wp3")
+	{
+		while (received==0)
+			{
+				if (previous==1)
+					{
+						movelow();
+						previous=0;
+					}
+				else if (previous ==0)
+					{
+						movehigh();
+						previous=1;
+					}
+			}
+		// elabora indizio
+		received=0;
+	}
+else if (msg->parameters[0].value == "wp4")
+	{
+		while (received==0)
+			{
+				if (previous==1)
+					{
+						movelow();
+						previous=0;
+					}
+				else if (previous ==0)
+					{
+						movehigh();
+						previous=1;
+					}
+			}
+		// elabora indizio
+		received=0;
+	}
+
+
+ROS_INFO("Action performed: completed!");
 	return true;
 	
 /*int random=rand()%10;
@@ -68,6 +156,8 @@ else
 int main(int argc, char **argv) {
 ros::init(argc, argv, "hintaction", ros::init_options::AnonymousName);
 ros::NodeHandle nh("~");
+ros::NodeHandle n;
+ros:: Subscriber reached= n.subscribe("/oracle_hint", 1000, hintCallback);
 KCL_rosplan::HintInterface my_aci(nh);
 my_aci.runActionInterface();
 ros::AsyncSpinner spinner(1);
